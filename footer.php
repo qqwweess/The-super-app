33c57c53ada89682503d98271d30063ac683b93a
qqwweess <67483386+qqwweess@users.noreply.github.com>
2024-11-18 09:19:31 +0300

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,45 @@
+
+<div class="container">
+    <div class="row footer-container text-center text-md-start">
+        <div class="col-md-3">
+            <div class="footer-title">KRAKEN.SHOP</div>
+            <p class="footer-description">У нас есть удочки, которые подходят и соответствуют вашему стилю которым вы гордитесь. От мужчин для мужчин.</p>
+        </div>
+        <div class="col-md-3">
+            <div class="footer-title">Компания</div>
+            <ul class="list-unstyled">
+                <li>О нас</li>
+                <li>Особенности</li>
+                <li>Работы</li>
+                <li>Карьера</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <div class="footer-title">Помощь</div>
+            <ul class="list-unstyled">
+                <li>Служба поддержки</li>
+                <li>Подробная информация</li>
+                <li>Правила и условия</li>
+                <li>Политика конфиденциальности</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <div class="footer-title">FAQ</div>
+            <ul class="list-unstyled">
+                <li>Вопросы</li>
+                <li>Заказы</li>
+                <li>Платежи</li>
+                <li>Поставки</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="footer-bottom d-flex justify-content-between align-items-center">
+        <div>Kraken.shop© 2024. Все права сохранены (наверное)</div>
+        <div class="footer-icons mt-2">
+            <img src="viza.svg" alt="Visa">
+            <img src="master.svg" alt="MasterCard">
+            <img src="mir.svg" alt="Мир">
+            <img src="sbp.svg" alt="СБП">
+        </div>
+</div>
