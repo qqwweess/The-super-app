c2ed80e39f6a31e89bb497d96894c631183f50e3
qqwweess <67483386+qqwweess@users.noreply.github.com>
2024-11-18 09:18:48 +0300

    Update header.php

---
+++ header.php
@@ -0,0 +1,35 @@
+
+<header class="bg-white shadow-sm">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <a class="navbar-brand fw-bold" href="#">KRAKEN.SHOP</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Магазин</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Новости</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Бренды</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="finder.svg" alt=""></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><img src="profile.svg" alt=""></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+</header>
+
